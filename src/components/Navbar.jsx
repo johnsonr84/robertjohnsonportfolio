991968404dbd600c7d615fffccc4c7cf38c8cc95
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-08 03:09:41 -0700

    update nav brand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
-            Robert &nbsp;
-            <span className="sm:block hidden"> | JavaScript Mastery</span>
+            Robert Johnson &nbsp;
+            <span className="sm:block hidden"> |  Portfolio</span>
           </p>
         </Link>
 
