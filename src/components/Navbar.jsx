b039555154b7be8f4cda8eee538ecda318dce10a
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-11 22:26:01 -0700

    change nav colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
             <li
               key={nav.id}
               className={`${
-                active === nav.title ? "text-white" : "text-secondary"
+                active === nav.title ? "text-grey" : "text-light"
               } hover:text-white text-[18px] font-medium cursor-pointer`}
               onClick={() => setActive(nav.title)}
             >
