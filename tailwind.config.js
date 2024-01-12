3d8c180c04385811a5a6e62e4ec94884ee326476
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-11 22:25:39 -0700

    add colors to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#050816",
-        secondary: "#aaa6c3",
+        light: "FFFFFF",
+        dark: "000000",
+        primary: "#ffffff",
+        secondary: "red",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
