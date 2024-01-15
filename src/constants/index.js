0d22d3080984c5066d2ea34e1c08c7a082562323
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-15 02:05:52 -0700

    add more testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 import {
-    mobile,
+    frontend,
     backend,
     creator,
     web,
@@ -31,7 +31,7 @@ import {
       title: "About",
     },
     {
-      id: "experience",
+      id: "work",
       title: "Experience",
     },
     {
@@ -46,8 +46,8 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
-      icon: mobile,
+      title: "Frontend Developer",
+      icon: frontend,
     },
     {
       title: "Backend Developer",
@@ -174,25 +174,49 @@ import {
       testimonial:
         "His dedication to software engineering is truly unbelievable. However, his track record makes his tale easier to swallow. I see Robert's progress as 2x to 4x not just from raw talent alone but his willingness to practice and learn.",
       name: "Scott Blanch",
-      designation: "Software Engineer",
+      designation: "Senior Software Engineer",
       company: "Ancestry",
       image: "https://media.licdn.com/dms/image/C5603AQHcHuxQnw7qCA/profile-displayphoto-shrink_400_400/0/1642100557504?e=1710374400&v=beta&t=-3IjvupA3FiN162RVFbC7N_O709C7Ok5RLn9WwAKb4U",
     },
     {
       testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
+        "In addition to his technical skills, Robert is also a supportive colleague who is always willing to lend a hand and share his knowledge. His positive attitude and collaborative approach make him a valuable asset to any team.",
+      name: "Brandon Schumacher",
+      designation: "Full Stack Developer",
+      company: "2U/ edX",
+      image: "https://media.licdn.com/dms/image/D5603AQHgYZNth1Hd6g/profile-displayphoto-shrink_100_100/0/1679866985106?e=1710979200&v=beta&t=bguHoTnu4kTXO7eutLFcxSBfVgiT0UMboG-HopqHc_0",
     },
     {
       testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
+        "He worked hard every day and put in extra hours during the 12-week cohort. He has a great background that allows him to easily interact and communicate with people across various industries.",
+      name: "Garret Clark",
+      designation: "Managing Director",
+      company: "Silicon Slopes",
+      image: "https://media.licdn.com/dms/image/C4D03AQFQpn4T4tvlDg/profile-displayphoto-shrink_100_100/0/1620687888579?e=1710979200&v=beta&t=2rCWb898yCvoQHLi8eq5JJBkc5ac5W1j_MAYYG90LZw",
+    },
+    {
+      testimonial:
+        "His mastery of everything related to Javascript and software development is awe-inspiring. I gained so much knowledge about coding, researching, and best practices, thanks to Rob. It was such an honor to work with him.",
+      name: "Sachi Takaoka",
+      designation: "JavaScript Developer",
+      company: "Bootcamp Student",
+      image: "https://media.licdn.com/dms/image/D5635AQGMfBmB3aJrAA/profile-framedphoto-shrink_100_100/0/1697253825820?e=1705914000&v=beta&t=FpUiXVtYNYTY9Ej0LV0ERHtk50MhnQ9RhaCwCd-A8n8",
+    },
+    {
+      testimonial:
+        "Rob is really smart and one of the most hardworking people I know. I've worked with him and watched him learn and grow as he's been studying at The Iron Yard and he is a quick learner and an excellent communicator.",
+      name: "Jason Larsen",
+      designation: "Software Engineering Manager",
+      company: "Mavenlink",
+      image: "https://media.licdn.com/dms/image/C5603AQHoNpvY7OMqBw/profile-displayphoto-shrink_100_100/0/1525626655549?e=1710979200&v=beta&t=Okd2oKzSaDamd4w8uuJL5BxCKxDN-LvvMd2zEF9tXlU",
+    },
+    {
+      testimonial:
+        "Rob has been such a delight to work with! Rob is always up for a challenge and always approaches problems with an open mind. I am always thrilled to work with Rob because he has such an excellent personality. Anyone would be extremely lucky to work with him.",
+      name: "Kelly Furness",
+      designation: "Software Engineer",
+      company: "Articulate",
+      image: "https://media.licdn.com/dms/image/C5603AQEtCW2yTQpLZw/profile-displayphoto-shrink_100_100/0/1653683341674?e=1710979200&v=beta&t=H__dslacnV-goaKUu8FC0hUDJGK96Xa9b5ktSnJMcsA",
     },
   ];
   
