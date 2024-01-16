0c7eb10551fd7502fa74668aac6cec781cdfe1a2
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-16 03:28:21 -0700

    add new testimonial

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -16,18 +16,17 @@ const FeedbackCard = ({
 }) => (
   <motion.div
     variants={fadeIn("", "spring", index * 0.5, 0.75)}
-    className='bg-[#fff] p-10 rounded-3xl xs:w-[320px] w-full'
+    className="bg-[#fff] p-10 rounded-3xl xs:w-[320px] w-full"
   >
-    <p className='text-black font-black text-[48px]'>"</p>
-    <div className='mt-1'>
-      <p className='text-black tracking-wider text-[15px]'>{testimonial}</p>
-
-      <div className='mt-7 flex justify-between items-center gap-1'>
-        <div className='flex-1 flex flex-col'>
-          <p className='text-grey font-medium text-[16px]'>
-            <span className='blue-text-gradient'>@</span> {name}
+    {/* <p className="text-black font-black text-[32px]">"</p> */}
+    <div className="mt-1">
+      <p className="text-black italic tracking-wider text-[15px]">"{testimonial}"</p>
+      <div className="mt-7 flex justify-between items-center gap-1">
+        <div className="flex-1 flex flex-col">
+          <p className="text-grey font-medium text-[15px]">
+            <span className="blue-text-gradient">@</span> {name}
           </p>
-          <p className='mt-1 text-secondary text-[12px]'>
+          <p className="mt-1 text-secondary text-[12px]">
             {designation} - {company}
           </p>
         </div>
@@ -35,7 +34,7 @@ const FeedbackCard = ({
         <img
           src={image}
           alt={`feedback_by-${name}`}
-          className='w-10 h-10 rounded-full object-cover'
+          className="w-10 h-10 rounded-full object-cover"
         />
       </div>
     </div>
@@ -46,7 +45,7 @@ const Feedbacks = () => {
   return (
     <div className={`mt-12 bg-[#2975f0] rounded-[20px]`}>
       <div
-        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[240px]`}
+        className={`bg-quaternary rounded-2xl ${styles.padding} min-h-[200px]`}
       >
         <motion.div variants={textVariant()}>
           <p className={styles.sectionSubTextLight}>What others say</p>
