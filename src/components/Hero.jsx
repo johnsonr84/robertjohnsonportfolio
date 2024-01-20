8cd37f5adb3c9953e3219cb9324caef9ecb51ca8
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-20 03:25:58 -0700

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,15 @@
 import { motion } from "framer-motion";
+// import { slideIn, fadeIn } from "../utils/motion";
 
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+// import { rob } from "../assets/";
+// import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`hero-img absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#2975f0]' />
@@ -18,19 +20,29 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-[#000]`}>
             Hi, I'm <span className='text-[#2975f0]'>Robert!</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-[#858585]`}>
+          <p className={`${styles.heroSubText} mt-1 text-[#858585]`}>
             A Fullstack Developer.
           </p>
-          <p className={`${styles.heroSubText} mt-2 text-[#858585]`}>
-            I develop software, user <br className='sm:block hidden' />
+          <p className={`${styles.heroSubText} mt-1 text-[#858585]`}>
+            I develop software, user <br />
             interfaces and web applications.
           </p>
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="h-[100-vh]"
+      >
+        <img
+          src={rob}
+          alt="hero section image of Robert standing at computer desk"
+          className="  object-cover"
+        />
+      </motion.div> */}
+      {/* <ComputersCanvas /> */}
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      {/* <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
@@ -46,7 +58,7 @@ const Hero = () => {
             />
           </div>
         </a>
-      </div>
+      </div> */}
     </section>
   );
 };
