17f76e23d94192680248cba71bd572c4305e93f1
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-20 03:26:30 -0700

    change spacing add socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,13 @@ import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
+import { BallCanvas } from "./canvas";
+import { socials } from "../constants";
 import { SectionWrapper } from "../hoc";
-import { slideIn } from "../utils/motion";
+import { slideIn, fadeIn } from "../utils/motion";
+import { rob2 } from "../assets/";
 
-const Contact = () => {
+const Contact = ({ index, title, }) => {
   const formRef = useRef();
   const [form, setForm] = useState({
     name: "",
@@ -66,11 +68,21 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+      className={`flex justify-center xl:mt-12 xl:flex-row flex-col gap-10 overflow-hidden`}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className="flex-[0.75] bg-[#454545] p-8 rounded-2xl"
+        className="flex justify-end xl:flex-1 xl:h-auto md:h-[550px] max-w-[350px]"
+      >
+        <img
+          src={rob2}
+          alt="contact section image of Robert holding laptop"
+          className="h-[auto] max-h-[800px] object-cover"
+        />
+      </motion.div>
+      <motion.div
+        variants={fadeIn("top", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-[#454545] p-8 rounded-2xl max-w-[500px] lg:min-w-[500px]"
       >
         <p className={styles.sectionSubTextLight}>Get in touch!</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -78,7 +90,7 @@ const Contact = () => {
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
+          className="flex mt-12 flex-col gap-8"
         >
           <label className="flex flex-col">
             <span className="text-white font-medium mb-4">Your Name</span>
@@ -123,12 +135,21 @@ const Contact = () => {
           </div>
         </form>
       </motion.div>
-
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+        className="flex justify-items-center items-center xl:flex-1 xl:h-auto md:h-[550px] max-w-[200px]"
       >
-        <EarthCanvas />
+        <div className="max-w-[200px]">
+          {socials.map((social, social_link, index) => (
+            <div
+              onClick={() => window.open(social_link, "_blank")}
+              className="cursor-pointer"
+              key={social.name}
+            >
+              <BallCanvas icon={social.icon} index={index} className="max-w-[200px]"/>
+            </div>
+          ))}
+        </div>
       </motion.div>
     </div>
   );
