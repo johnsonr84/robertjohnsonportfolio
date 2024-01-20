6a37178847a9642898cae7306c8041a8e4567411
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-20 03:24:55 -0700

    update styles

--- src/styles.js
+++ src/styles.js
@@ -4,9 +4,9 @@ const styles = {
     padding: "sm:px-16 px-6 sm:py-16 py-10",
   
     heroHeadText:
-      "text-[#545454] lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+      "text-[#545454] xl:text-[56px] lg:text-[48px] sm:text-[28px] text-[22px] lg:leading-[68px] mt-2",
     heroSubText:
-      "text-[#434344] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+      "text-[#434344] font-medium xl:text-[22px] lg:text-[18px] md:text-[16px] sm:text-[14px] text-[12px] lg:leading-[40px]",
   
     sectionHeadText:
       "text-[#2975f0] font-[] md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
