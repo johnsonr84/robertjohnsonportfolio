d89c4b705cc4fb82a49e3debf3c2d506636c4db1
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-21 23:52:55 -0700

    add typewriter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,38 @@
 import { motion } from "framer-motion";
 // import { slideIn, fadeIn } from "../utils/motion";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 
 import { styles } from "../styles";
 // import { rob } from "../assets/";
 // import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const [text, count] = useTypewriter({
+    words: [
+      'Hey! I am Rob, A Fullstack Developer. Welcome to my portfolio!',
+      'I enjoy building software, user interfaces and web applications.',
+    ],
+    loop: true,
+    typeSpeed: 80,
+    delaySpeed: 1000,
+    deleteSpeed: 50,
+  })
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
         className={`hero-img absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
+        {/* <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#2975f0]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
-        </div>
+        </div> */}
 
-        <div>
-          <h1 className={`${styles.heroHeadText} text-[#000]`}>
+        <div className="text-center">
+        <h1 className="text-3xl lg:text-3xl px-5 p-2 max-w-2lg text-center text-[#545454]">
+          <span className="mr-3 text-[10px] md:text-xl base:text-2xl lg:text-3xl xl:text-3xl">{text}</span>
+          <Cursor cursorColor="#2975f0" />
+        </h1>
+          {/* <h1 className={`${styles.heroHeadText} text-[#000]`}>
             Hi, I'm <span className='text-[#2975f0]'>Robert!</span>
           </h1>
           <p className={`${styles.heroSubText} mt-1 text-[#858585]`}>
@@ -26,7 +41,7 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-1 text-[#858585]`}>
             I develop software, user <br />
             interfaces and web applications.
-          </p>
+          </p> */}
         </div>
       </div>
 
