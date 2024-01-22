7e0a9c9af843640d93677540a5f300243002fc02
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-22 01:16:29 -0700

    add heading and sub heading

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,9 +4,20 @@ import { motion } from "framer-motion";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { textVariant } from "../utils/motion";
+import { styles } from "../styles";
 
 const Tech = ({index, title}) => {
   return (
+    <div>
+        <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          Not all the tools, but the primary ones!
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Dev Toolbox.
+        </h2>
+      </motion.div>
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology, index) => (
         <div className='w-28 h-28' key={technology.name}>
@@ -14,6 +25,8 @@ const Tech = ({index, title}) => {
         </div>
       ))}
     </div>
+    </div>
+
   );
 };
 
