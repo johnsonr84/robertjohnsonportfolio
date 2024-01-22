b0b8d9ee8ac90e773ec92a7e3a1df20e71592069
Dev Dork <robertcjohnson1984@gmail.com>
2024-01-21 23:50:20 -0700

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full rounded-[20px] border shadow-card'
+      className='w-full rounded-[20px]'
       style={{
         borderRadius: "20px",
-        // border: "2px solid grey"
+        border: "2px solid #cccccc"
       }}
     >
       <div
@@ -23,7 +23,7 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
-        className='bg-white rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        className='bg-[#fff] rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
         <img
           src={icon}
@@ -31,7 +31,7 @@ const ServiceCard = ({ index, title, icon }) => (
           className='w-24 h-24 object-contain'
         />
 
-        <h3 className='text-[#545454] text-[20px] font-bold text-center'>
+        <h3 className='text-[#2975f0] text-[20px] font-bold text-center'>
           {title}
         </h3>
       </div>
@@ -49,9 +49,9 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-[#545454] text-[17px] max-w-3xl leading-[30px]'
+        className='mt-4 text-[#545454] text-[17px] max-w-6xl leading-[30px]'
       >
-       I am a skilled Fullstack Software Developer with expertise in HTML, CSS, JavaScript, React, Angular, TypeScript, Node.js, Express.js, Python, and C#. Proficient in database management, RESTful APIs, and Git, I deliver end-to-end solutions. Committed to creating high-quality applications, I tackle complex challenges, ensuring project success. A quick learner, I collaborate to create efficient, scalable, user-friendly solutions. Let's bring your ideas to life!"
+       I am a Fullstack Software Developer proficient in HTML, CSS, JavaScript, React, Angular, TypeScript, Node.js, Express.js, Python, and C#. With a strong command of database management, RESTful APIs, and Git, I specialize in delivering comprehensive end-to-end solutions. My commitment to crafting high-quality applications empowers me to tackle intricate challenges, ensuring project success. As a quick learner, I thrive on collaboration to create efficient, scalable, and user-friendly solutions. Let's transform your ideas into reality!"
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
